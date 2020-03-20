efa9e25164ebc8e1f5b2f7bb7a7dd319723ce9fe
geocguy <sq92219@msn.cn>
2020-03-21 00:08:44 +0800

    更新第一篇文章

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome to My World",'url':"/2020/03/20/%E6%88%91%E7%9A%84%E7%AC%AC%E4%B8%80%E7%AF%87%E6%96%87%E7%AB%A0.html"}]};
